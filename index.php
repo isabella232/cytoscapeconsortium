28f297fe0c143797cee3e978bf36d3f154542036
Barry Demchak <idekerlab.bdemchak@gmail.com>
2018-03-26 16:53:20 -0700

    Added D&B number

--- index.php
+++ index.php
@@ -110,6 +110,7 @@ article {
 								<p class="wp-caption-text">Figure 1. Projects of the Cytoscape Consortium</p>
 							</div>
 
+							<p><b>D&B (DUNS) number:</b> 022073749</b>
 							<p>To donate via Paypal:</p>
                             <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                                 <input type="hidden" name="cmd" value="_s-xclick">
